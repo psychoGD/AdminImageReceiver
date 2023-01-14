59ecd095cb5cb27f3a0eafcdf51d8d03663906bc
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-01-15 01:15:15 +0400

    Add project files.

---
+++ AdminImageReciever/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+//using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+using System.IO;
+using System.Drawing;
+
+namespace AdminImageReciever
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //private readonly object _personCollectionLock;
+        private readonly object _locker = new object();
+        private ObservableCollection<Image> _imagesFromUsers;
+
+        public ObservableCollection<Image> ImagesFromUsers
+        {
+            get { return _imagesFromUsers; }
+            set
+            {
+                _imagesFromUsers = value;
+                BindingOperations.EnableCollectionSynchronization(_imagesFromUsers,_locker);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            ImagesFromUsers = new ObservableCollection<Image>();
+            Task.Run(() =>
+            {
+                ServerStart();
+            });
+            
+        }
+        public Image byteArrayToImage(byte[] bytesArr)
+        {
+            using (MemoryStream memstr = new MemoryStream(bytesArr))
+            {
+                Image img = Image.FromStream(memstr);
+                
+                return img;
+            }
+        }
+        public void ServerStart()
+        {
+            var ipAddress = IPAddress.Parse("192.168.56.1");
+            var port = 80;
+            using(var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipAddress, port);
+                socket.Bind(ep);
+                socket.Listen(10);
+                MessageBox.Show("Server Is Running");
+                while (true)
+                {
+                    var client = socket.AcceptAsync().Result;
+                    Task.Run(() =>
+                    {
+                        var length = 0;
+                        var bytes = new byte[50000];
+                        do
+                        {
+                            length = client.Receive(bytes);
+                            //MessageBox.Show(length.ToString());
+                            //Image x = byteArrayToImage(bytes);
+                            using (var ms = new MemoryStream(bytes))
+                            {
+                                var image = Image.FromStream(ms);
+                                ImagesFromUsers.Add(image);
+                            }
+                        } while (true);
+                    });
+                }
+            }
+        }
+    }
+}
